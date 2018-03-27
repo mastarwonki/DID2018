29fac31aeca50fd58d8b52cc5947dab478941af4
mastarwonki <mastarwonki@hotmail.it>
2018-03-27 17:26:59 +0200

    Fragments + service(working)

--- app/src/main/java/com/example/ronk1/lamp_es/Tab1_Lamp.java
+++ app/src/main/java/com/example/ronk1/lamp_es/Tab1_Lamp.java
@@ -1,5 +1,6 @@
 package com.example.ronk1.lamp_es;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -33,17 +34,21 @@ import java.util.ArrayList;
  * Created by irene on 30/12/2017.
  */
 
-public class Tab1_Lamp extends Fragment {
+public class Tab1_Lamp extends Fragment implements View.OnClickListener, SeekBar.OnSeekBarChangeListener, Switch.OnCheckedChangeListener{
 
     //lamp infos
     private int pos;
     private String ip;
+    private Lamp activeLamp;
 
     //variables
+    //GUI
+    private Switch switch1;
+    private Button b1;
+    private Button b2;
+    private Button b3;
     private LampView lv;
     private SeekBar sb, seekBar;
-    //private TcpClient tcpClient;
-    //private ConnectTask connectTask;
 
     //default messages
     private final String turnOn = "turnOn";
@@ -59,7 +64,8 @@ public class Tab1_Lamp extends Fragment {
 
     //service
     private TcpService myService;
-    boolean mBound = false;
+    private boolean mBound = false;
+    private TcpService.OnMessageReceived messageReceived = null;
 
     @Override
     public void onStart() {
@@ -92,21 +98,21 @@ public class Tab1_Lamp extends Fragment {
 
         LampManager lm = LampManager.getInstance();
         final ArrayList<Lamp> lamps = (ArrayList<Lamp>) lm.getLamps();
-        final Lamp activeLamp = lamps.get(pos);
+        activeLamp = lamps.get(pos);
 
-        final Button b1 = view.findViewById(R.id.button);
-        final Button b2 = view.findViewById(R.id.button2);
-        final Button b3 = view.findViewById(R.id.button3);
-        final Switch switch1 = view.findViewById(R.id.switch1);
+        b1 = view.findViewById(R.id.button);
+        b2 = view.findViewById(R.id.button2);
+        b3 = view.findViewById(R.id.button3);
+        switch1 = view.findViewById(R.id.switch1);
 
         if(activeLamp.isOn())
             switch1.setChecked(true);
         else
             switch1.setChecked(false);
 
-        if(mBound) {
+        //if(mBound) {
 
-            myService.setMessageListener(new TcpService.OnMessageReceived() {
+            messageReceived = new TcpService.OnMessageReceived() {
                 @Override
                 public void messageReceived(final String message) {
                     //this method calls the onProgressUpdate
@@ -115,7 +121,6 @@ public class Tab1_Lamp extends Fragment {
                     Runnable myRunnable = new Runnable() {
                         @Override
                         public void run() {
-
 
                             // TODO Switch-method to set Lamp Attributes (board packets)
                             Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
@@ -141,6 +146,17 @@ public class Tab1_Lamp extends Fragment {
                                         seekBar.setProgress((activeLamp.getIntensity() * seekMax) / maxLum);
                                     }
                                     break;
+
+                                case "Connection Refused":
+                                    getActivity().finish();
+                                    break;
+
+                                case "Connection Stopped":
+                                    getActivity().finish();
+                                    break;
+
+                                default:
+                                    break;
                             }
 
                         }
@@ -148,88 +164,26 @@ public class Tab1_Lamp extends Fragment {
                     mainHandler.post(myRunnable);
 
                 }
-            });
-            
+            };
+
             TextView tv = view.findViewById(R.id.lamp_name);
             tv.setText(activeLamp.getName());
             ImageView iv = view.findViewById(R.id.lampada);
             iv.setImageBitmap(activeLamp.getPicture());
 
-            b1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+            b1.setOnClickListener(this);
+            b2.setOnClickListener(this);
+            b3.setOnClickListener(this);
 
-                    b1.setBackground(ContextCompat.getDrawable(context, R.drawable.focus_light_selected));
-                    b2.setBackground(ContextCompat.getDrawable(context, R.drawable.neutral_light));
-                    b3.setBackground(ContextCompat.getDrawable(context, R.drawable.relax_light));
-
-                }
-            });
-
-            b2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    b1.setBackground(ContextCompat.getDrawable(context, R.drawable.focus_light));
-                    b2.setBackground(ContextCompat.getDrawable(context, R.drawable.neutral_light_selected));
-                    b3.setBackground(ContextCompat.getDrawable(context, R.drawable.relax_light));
-
-                }
-            });
-
-            b3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    b1.setBackground(ContextCompat.getDrawable(context, R.drawable.focus_light));
-                    b2.setBackground(ContextCompat.getDrawable(context, R.drawable.neutral_light));
-                    b3.setBackground(ContextCompat.getDrawable(context, R.drawable.relax_light_selected));
-                }
-            });
-
-            switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    if (switch1.isChecked()) {
-                        //if (tcpClient != null) {
-                        myService.setMessage("turnOn");
-                        //}
-                        activeLamp.turnOn();
-                    } else {
-                        //if (tcpClient != null) {
-                        myService.setMessage("turnOff");
-                        //}
-                        activeLamp.turnOff();
-                    }
-                }
-            });
+            switch1.setOnCheckedChangeListener(this);
 
             seekBar = view.findViewById(R.id.seekBar);
             seekBar.setMax(seekMax);
             if (activeLamp.getIntensity() != 0)
                 seekBar.setProgress(activeLamp.getIntensity() / lumStep);
             else seekBar.setProgress(0);
-            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-
-                    currentProgress = progress * lumStep;
-                    activeLamp.setIntensity(currentProgress);
-
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                    myService.setMessage(setIntensity + "," + activeLamp.getIntensity());
-
-                }
-            });
-        }
+            seekBar.setOnSeekBarChangeListener(this);
+       // }
 
 
         return view;
@@ -239,6 +193,7 @@ public class Tab1_Lamp extends Fragment {
         public void onServiceConnected(ComponentName className, IBinder service) {
             TcpService.MyLocalBinder binder = (TcpService.MyLocalBinder) service;
             myService = binder.getService();
+            myService.setMessageListener(messageReceived);
             mBound = true;
         }
         public void onServiceDisconnected(ComponentName arg0) {
@@ -248,4 +203,64 @@ public class Tab1_Lamp extends Fragment {
     };
 
 
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+
+            case R.id.button:
+                b1.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.focus_light_selected));
+                b2.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.neutral_light));
+                b3.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.relax_light));
+                break;
+
+            case R.id.button2:
+                b1.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.focus_light));
+                b2.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.neutral_light_selected));
+                b3.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.relax_light));
+                break;
+
+            case R.id.button3:
+                b1.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.focus_light));
+                b2.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.neutral_light));
+                b3.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.relax_light_selected));
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+
+        currentProgress = progress * lumStep;
+        activeLamp.setIntensity(currentProgress);
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        myService.setMessage(setIntensity + "," + activeLamp.getIntensity());
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+        if (switch1.isChecked()) {
+            myService.setMessage("turnOn");
+            activeLamp.turnOn();
+        } else {
+            myService.setMessage("turnOff");
+            activeLamp.turnOff();
+        }
+
+    }
 }
